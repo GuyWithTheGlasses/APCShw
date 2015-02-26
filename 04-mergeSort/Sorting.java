071bfc10b9b003e1efa6f6e6b722d3e2daf35e04
GuyWithTheGlasses <kevin.a.yan@gmail.com>
2015-02-26 01:16:39 -0500

    Worked on merge, still throws IndexOutOfBoundsError

--- 04-mergeSort/Sorting.java
+++ 04-mergeSort/Sorting.java
@@ -1,4 +1,4 @@
-import java.util.*
+import java.util.*;
 
 public class Sorting{
 
@@ -6,8 +6,8 @@ public class Sorting{
     private ArrayList<Integer> nums;
     private Random rnd = new Random();
 
-    public Sorting(int l){
-	listlength = l;
+    public Sorting(){
+	listlength = 10;
 	nums = new ArrayList<Integer>();
 	for(int i = 0 ; i < listlength ; i++){
 	    nums.add(rnd.nextInt(listlength));
@@ -21,11 +21,36 @@ public class Sorting{
 	for(int i = 0 ; i < a.size() + b.size() ; i++){
 	    int valueA = a.get(placeA);
 	    int valueB = b.get(placeB);
-	    if(valueA > valueB){
-
+	    if(valueA < valueB){
+		mergedlist.add(valueA);
+		placeA++;
+	    }
+	    else{
+		mergedlist.add(valueB);
+		placeB++;
 	    }
 	}
 	return mergedlist;     
     }
 
+    public String toString(ArrayList<Integer> al){
+	String str = "{";
+	for(int i = 0 ; i < al.size() ; i++){
+	    str = str + al.get(i) + " ,";
+	}
+	str = str + "}";
+	return str;
+    }
+
+    public static void main(String[] args){
+	Sorting s = new Sorting();
+	ArrayList<Integer> al1 = new ArrayList<Integer>();
+	ArrayList<Integer> al2 = new ArrayList<Integer>();
+	for(int i = 0 ; i < 5 ; i++){
+	    al1.add(i);
+	    al2.add(i+1);
+	}
+	System.out.println(s.merge(al1,al2));
+	
+    }
 }
