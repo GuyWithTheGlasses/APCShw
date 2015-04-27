f6b5da376d224e1c553df444ec709f3c22c3798a
GuyWithTheGlasses <kevin.a.yan@gmail.com>
2015-04-27 09:26:45 -0400

    Still working on traverse

--- 14-trees/Tree.java
+++ 14-trees/Tree.java
@@ -12,66 +12,108 @@ public class Tree{
 	return root;
     }
 
+    /*--------------------- Insert and Search Methods ------------------------*/
+    /*
     public void insert(Node groot, int i){
 	//Inserts a node with data i in the proper location on the tree
 	//with root pointing to groot
 	Node n = new Node(i);
-
 	//Base case where our tree is empty
-	if(groot == null)
+	if(groot == null){
 	    groot = n;
-
+	    return;
+	}
 	//Otherwise, we have to search through the tree
+	Node t = groot;
+	Node t2 = null; //piggybacking
+	while(t != null){
+	    t2 = t;
+	    //int c = t.getData().compareTo(i);
+	    //"int cannot be dereferenced in java" ...what?
+	    if(i == t.getData())
+		return;
+	    else if(i > t.getData())
+		t = t.getRight();
+	    else if(i < t.getData())
+		t = t.getLeft();
+	    else
+		return;
+	}
+	//Insert new node on left or right depending on the value of i
+	if(i > t2.getData())
+	    t2.setRight(n);
+	else
+	    t2.setLeft(n);
+    }
+    */
+
+    public void insert(Node r, int i){
+	Node n = new Node(i);
+	Node t2 = null;
+	Node t = r;
+	if (r == null){
+	    r = n;
+	    return;
+	}						
+	while (t!=null){
+	    t2 = t;
+	    if (t.getData() == i)
+		return;
+	    else if (t.getData() < i)
+		t = t.getRight();
+	    else if (t.getData() > i)
+		t = t.getLeft();
+	    else
+		return;
+	}				
+	if (i > t2.getData())
+	    t2.setRight(n);
+	else
+	    t2.setLeft(n);
+    }
+
+    public Node rsearch(Node groot, int i){
+	//Recursive form of the search
+	if(groot == null || groot.getData() == i)
+	    return groot;
+	else if(i > groot.getData())
+	    return rsearch(groot.getRight(), i);
+	else
+	    return rsearch(groot.getLeft(), i);	    
+    }
+
+    public String search(int i){
+        Node retval = rsearch(root,i);
+	if(retval == null)
+	    return "not found";
+	else
+	    return ""+retval;
+    }
+
+    public String traverse(Node groot){
+	String s = "";
+	if(groot == null)
+	    return s;
 	else{
-	    Node t = groot;
-	    Node t2 = t; //piggybacking
-	    
-	    boolean onLeft = false;
-	    //so we know whether we add on the left or right of t2
-	    
-	    while(t != null){
-		t2 = t;
-		//int c = t.getData().compareTo(i);
-		//"int cannot be dereferenced in java" ...what?
-		if(i > t.getData()){
-		    t = t.getRight();
-		    onLeft = false;
-		}
-		else if(i < t.getData()){
-		    t = t.getLeft();
-		    onLeft = true;
-		}
-	    }
-	    if(onLeft)
-		t2.setLeft(n);
-	    else
-		t2.setRight(n);
-	}
+	    s = s + groot.getData();
+	    s = s + traverse(groot.getLeft());
+	    s = s + traverse(groot.getRight());
+	    return s;
+	}	
+    }
+    
+    public String toString(){
+	return traverse(root);
     }
 
-    public Node search(Node groot, int i){
-	//Finds a node with data i and returns it
-	//Else returns null
-	Node tmp = groot;
-	while(tmp != null){
-	    int cur = tmp.getData();
-	    System.out.println(tmp);
-	    if(i == cur)
-		return tmp;
-	    else if(i > cur)
-		tmp = tmp.getRight();
-	    else
-		tmp = tmp.getLeft();
-	}
-	return null;		    
-    }
-
+    /*-------------------------------- Main --------------------------------*/
+    
     public static void main(String[] args){
 	Tree t = new Tree();
-	for(int i = 0 ; i < 10; i--)
+	for(int i = 0 ; i < 10; i++)
 	    t.insert(t.getRoot(),i);
-	t.insert(t.getRoot(),11);
-	t.search(t.getRoot(),11);
+	System.out.println(t.search(5));
+	System.out.println(t);
     }
     
 }
