66edd079c0e42004af91b2f27d79297cab1a2543
GuyWithTheGlasses <kevin.a.yan@gmail.com>
2015-04-29 09:26:59 -0400

    Started delete method

--- 14-trees/Tree.java
+++ 14-trees/Tree.java
@@ -1,56 +1,25 @@
 import java.util.*;
 
 public class Tree{
-
     private Node root;
 
     public Tree(){
 	root = null;
     }
 
-    /*--------------------- Insert and Search Methods ------------------------*/
-    /*
-    public void insert(Node groot, int i){
-	//Inserts a node with data i in the proper location on the tree
-	//with root pointing to groot
-	Node n = new Node(i);
-	//Base case where our tree is empty
-	if(groot == null){
-	    groot = n;
-	    return;
-	}
-	//Otherwise, we have to search through the tree
-	Node t = groot;
-	Node t2 = null; //piggybacking
-	while(t != null){
-	    t2 = t;
-	    //int c = t.getData().compareTo(i);
-	    //"int cannot be dereferenced in java" ...what?
-	    if(i == t.getData())
-		return;
-	    else if(i > t.getData())
-		t = t.getRight();
-	    else if(i < t.getData())
-		t = t.getLeft();
-	    else
-		return;
-	}
-	//Insert new node on left or right depending on the value of i
-	if(i > t2.getData())
-	    t2.setRight(n);
-	else
-	    t2.setLeft(n);
-    }
-    */
+    /*------------------- Insert and Search Methods ----------------------*/
 
     public void insert(int i){
 	Node n = new Node(i);
-	Node t2 = null;
+	Node t2 = null; //Piggyback nodes
 	Node t = root;
+	//If root is null, well we just insert the node there
 	if (root == null){
 	    root = n;
 	    return;
-	}						
+	}
+	//If it's not null, we move left/right depending on the value of i
+	//to find the proper location to insert the nodex
 	while (t!=null){
 	    t2 = t;
 	    if (t.getData() == i)
@@ -61,7 +30,9 @@ public class Tree{
 		t = t.getLeft();
 	    else
 		return;
-	}				
+	}
+	//By now, t should point the location the node should go
+	//And t2 should point at the node right before it
 	if (i > t2.getData())
 	    t2.setRight(n);
 	else
@@ -79,6 +50,7 @@ public class Tree{
     }
 
     public String search(int i){
+	//For convieniece's sake
         Node retval = rsearch(root,i);
 	if(retval == null)
 	    return "not found";
@@ -86,23 +58,63 @@ public class Tree{
 	    return ""+retval;
     }
 
+    /*------------------------ How to toString ---------------------------*/
+
     public String traverse(Node groot){
 	String s = "";
-	if(groot == null)
-	    return s;
-	else{
-	    s = s + traverse(groot.getLeft())+" ";
-	    s = s + groot.getData()+" ";
-	    s = s + traverse(groot.getRight())+" ";
-	    return s;
-	}	
+	if(groot != null)
+	    s = s + traverse(groot.getLeft()) +
+		groot.getData() +", " +
+		traverse(groot.getRight());
+	//Intuitively, this always prints in order
+	//Because we do the left first always, which is smaller
+	//This is known as an in-order traversal
+	return s;
     }
-    
+
     public String toString(){
 	return traverse(root);
     }
 
-    /*-------------------------------- Main --------------------------------*/
+    /*------------------------- Finally, Delete --------------------------*/
+    /*
+      Three cases:
+      1. Remove a leaf
+       - point the previous node to null
+      2. Node has one child
+       - point the previous node to the current node's child
+      1 and 2 can basically be combined into one algorithm.
+
+      3. Node has two children 
+       - find largest node on left subtree / smallest on right subtree
+         *Go as far to the right as you can on the left
+         *This node always has, at most, 1 child
+       - copy that node's data into the space left by the deleted node
+       - set the previous node to null
+     */
+    public void delete(){
+	Node i = new Node(i);
+	Node t2 = null;
+	Node t = root;
+	while(t != null){
+	    t2 = t;
+	    if (t.getData() == i)
+	        break;
+	    else if (t.getData() < i)
+		t = t.getRight();
+	    else if (t.getData() > i)
+		t = t.getLeft();
+	    else
+		return;
+	}
+	//Same algorithm as insert, t2 points at the node before our target
+	if(t.getLeft() == null)
+	    t2.setRight(t.getRight());
+	else if(t.getRight() == null)
+	    t2.setLeft(t.getLeft());
+	//More is needed, not done yet!
+
+	/*------------------------------ Main --------------------------------*/
     
     public static void main(String[] args){
 	Tree t = new Tree();
